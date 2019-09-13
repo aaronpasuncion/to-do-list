e2cea0faec46c35c256375ba554e5f891210ab87
Aaron P. Asuncion <aaronpasuncion@gmail.com>
2019-09-13 15:10:50 -0400

    Updates

---
+++ precache-manifest.000f06b4f88786e3dba7a082e803a738.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134f4f6d9282bd4042123efe0882f928",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "82b9ddb519094b201384",
+    "url": "/to-do-list/static/css/main.8ba408e3.chunk.css"
+  },
+  {
+    "revision": "e2eba27ed50f054c8cb6",
+    "url": "/to-do-list/static/js/2.e0122986.chunk.js"
+  },
+  {
+    "revision": "82b9ddb519094b201384",
+    "url": "/to-do-list/static/js/main.f9f3ec14.chunk.js"
+  },
+  {
+    "revision": "6eb42f32bf833768c169",
+    "url": "/to-do-list/static/js/runtime~main.cc244b0f.js"
+  }
+]);
